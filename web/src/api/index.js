78a76d0c6e0911812fa68a81bce90305a224c03a
fanwenlong <wenlong.fan@ebestmobile.com>
2020-01-09 10:40:12 +0800

    修改地址

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,7 +1,7 @@
 import XLSX from 'xlsx'
 import transformSheets from '@/api/Excel.js'
 // 配置API接口地址
-var root = 'http://112.126.99.55:8080/'
+var root = 'http://112.126.99.55/'
 // 引用axios
 var axios = require('axios')
 // 自定义判断元素类型JS
